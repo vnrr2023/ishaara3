602205145e91bdcacd27820a3749a4e1cd782f40
rsayyed591 <rehansayyed591@gmail.com>
2024-12-11 18:56:23 +0530

    first commit 42

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,10 @@ import Spline from '@splinetool/react-spline/next';
 
 export default function Home() {
   return (
-    <main>
+    <main className="h-screen bg-black flex items-center justify-center">
       <Spline
-        scene="https://prod.spline.design/XrWz5x7Wpm29cGo6/scene.splinecode" 
+        scene="https://prod.spline.design/XrWz5x7Wpm29cGo6/scene.splinecode"
+        className="w-full h-full"
       />
     </main>
   );
